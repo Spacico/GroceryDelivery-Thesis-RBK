0776cfa0395b9209a0d46d99a6e68fa54a1aa2c9
AhmadHaririrbk <eng.ahma1983@gmail.com>
2017-10-29 15:38:37 -0400

    server with sign up and login consumer fixed

--- database/model/Agents.js
+++ database/model/Agents.js
@@ -1,7 +1,6 @@
 //this Model deal with users data
-// var bcrypt = require('bcrypt-nodejs');
-var Promise = require('bluebird');
 var mongoose = require('mongoose');
+
 var AgentsSchema = mongoose.Schema({
     agentName: { type: String, index: { unique: true } },
     password: String,
@@ -12,14 +11,7 @@ var AgentsSchema = mongoose.Schema({
     firstName: String,
     lastName: String
 });
+
 var Agents = mongoose.model('Agents', AgentsSchema);
-//this for encrypt password data
-// AgentsSchema.pre('save', function(next) {
-//   var cipher = Promise.promisify(bcrypt.hash);
-//   return cipher(this.password, null, null).bind(this)
-//   .then(function(hash) {
-//     this.password = hash;
-//     next();
-//   });
-// });
+
 module.exports = Agents;
