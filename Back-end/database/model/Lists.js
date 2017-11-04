7a268060a51c495832ac568ab3c817b61210547e
AhmadHaririrbk <eng.ahma1983@gmail.com>
2017-11-04 11:31:33 -0400

    Fix conflict at file database/modal/Lists.js

--- Back-end/database/model/Lists.js
+++ Back-end/database/model/Lists.js
@@ -2,7 +2,6 @@
 var mongoose = require('mongoose');
 
 var listSchema = mongoose.Schema({
-<<<<<<< HEAD:Back-end/database/model/Lists.js
 	agentName:  String,
 	consumerName: String,
 	items:String,
@@ -12,19 +11,7 @@ var listSchema = mongoose.Schema({
 		longitude:Number
 	},
 	storeInfo:String
-})
-=======
-    agentName: String,
-    consumerName: String,
-    items: String,
-    available: Boolean,
-    location: {
-        latitude: Number,
-        longitude: Number
-    },
-    storeInfo: String
 });
->>>>>>> a12e2d41bdfd2b77e6a99319b3bda0992ebd8677:database/model/Lists.js
 
 var Lists = mongoose.model('Lists', listSchema);
 
