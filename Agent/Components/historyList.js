011c2c2cfd0cf67df7b1eb64b00d2bd72a23eb57
DoaaAlsafadi <d.alsafadi.jo@gmail.com>
2017-11-08 21:32:22 +0200

    fix dimentions on historyList

--- Agent/Components/historyList.js
+++ Agent/Components/historyList.js
@@ -7,7 +7,8 @@ import {
   Text,
   AppRegistry,
   Button,
-  TouchableOpacity
+  TouchableOpacity,
+  Dimensions
 } from 'react-native';
 //import Mapo from './mapView';
 import getDirections from 'react-native-google-maps-directions';
@@ -55,7 +56,7 @@ export default class HistoryList extends Component {
                 <Text
                   style={styles.val}>
                   <Text style={styles.key}>Items:</Text>
-                  {this.state.list.items}
+                  {'  ' + this.state.list.items}
                 </Text>
 
                 <Text
@@ -63,7 +64,7 @@ export default class HistoryList extends Component {
                   <Text style={styles.key}>
                     Consumer name:
                   </Text>
-                  {this.state.list.consumerName}
+                  {'  ' + this.state.list.consumerName}
                 </Text>
 
                 <Text
@@ -71,7 +72,7 @@ export default class HistoryList extends Component {
                   <Text style={styles.key}>
                     Store information:
                   </Text>
-                  {this.state.list.storeInfo}
+                  {'  ' + this.state.list.storeInfo}
                 </Text>
                 </View>
           </Modal>
@@ -81,63 +82,51 @@ export default class HistoryList extends Component {
   }
 }
 
+const {width, height} = Dimensions.get('window')
 const styles = StyleSheet.create({
   container: {
+    marginTop: 20,
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
-  // listInfoContainer: {
-  //   backgroundColor: '#B5B5B5',
-  //   padding: 5,
-  //   margin: 5
-  // },
-  // buttonContainer: {
-  //   margin: 20
-  // },
-  // alternativeLayoutButtonContainer: {
-  //   margin: 20,
-  //   flexDirection: 'row',
-  //   justifyContent: 'space-between'
-  // },
   list :{
     marginTop:10,
     flex: 1,
     justifyContent: 'center',
     backgroundColor : '#6668d0',
-    width:300,
-    height:60,
+    width:width * 0.7,
+    height:height * 0.1,
     borderRadius:10,
     justifyContent: 'center',
     alignItems: 'center',
   },
   text:{
     color:'white',
-    fontSize:20,
+    fontSize:22,
     fontWeight:'bold',
     justifyContent: 'center',
     alignItems: 'center',
   },
   val:{
-      marginTop:25,
+    marginTop:25,
     color:'#26418f',
     fontSize:20,
     fontWeight:'bold',
-
   },
   key:{
     marginTop:25,
     color:'white',
-    fontSize:25,
+    fontSize:20,
     fontWeight:'bold',
   },
   modal:{
-    marginLeft:8,
-    width:400,
-    height:250,
-    marginTop:150,
-    borderRadius:10,
-    borderWidth:2,
+    width: width * 0.9,
+    height: height * 0.3,
+    marginTop:25,
+    margin:20,
+    padding: 5,
+    borderRadius:15,
     backgroundColor:'#6668d0',
   }
 });
